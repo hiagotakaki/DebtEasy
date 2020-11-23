3e292660d667d5443ae0555244887e4fddcd0e21
Hiago Takaki <hiagotakaki@outlook.com>
2020-11-22 22:28:26 -0300

    Estágio Inicial do Front-End Base; Estruturação inicial

---
+++ ClientApp/src/app/debitos/debitos.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-debitos',
+  templateUrl: './debitos.component.html',
+  styleUrls: ['./debitos.component.css']
+})
+export class DebitosComponent implements OnInit {
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
